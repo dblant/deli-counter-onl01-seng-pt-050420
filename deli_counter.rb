04e5145983bf661ee665598266b2dd9d3e13e785
Dustin Blanton <dustinblntn@gmail.com>
2020-05-13 01:05:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(deli)
   if deli.length == 0
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     current_line = "The line is currently:"
     deli.map.with_index(1) do |customer, index|
